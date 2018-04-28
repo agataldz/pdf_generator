366d6910ef90ea2f08d682a5697690a7ff227ef3
agataldz <agataldzz@gmail.com>
2018-04-28 16:36:41 +0200

    forms, template

--- pdf_app/forms.py
+++ pdf_app/forms.py
@@ -6,4 +6,8 @@ from pdf_app.models import Pdf
 class PdfForm(forms.ModelForm):
     class Meta:
         model = Pdf
+<<<<<<< HEAD
         exclude = ['file']
+=======
+        fields = '__all__'
+>>>>>>> 4bfc297ccbc58127945e3cd8a15b9bc0cafcac09
